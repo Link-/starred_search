80c1983fa04f474d4ec9b9ca8ef2464478c0fda5
Bassem Dghaidi <link-@github.com>
2020-08-01 14:35:22 +0200

    Add help option

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,39 @@
 import arg from 'arg'
 const main = require('./main.js');
 
+const help = `
+Usage: starred_search [OPTIONS] [ARGS]...
+
+  Search your or any other user's starred repositories on GitHub for a keyword.
+
+Options:
+  -h, --help
+    Show this message and exit.
+  
+  -u, --user <handle>
+    Any GitHub handle. Example: link-
+
+  -c, --cache-dir <directory>
+    Directory you want to store the cache file in. Example: /tmp/.cache
+
+  -f, --find <keyword>
+    The keyword you want to search for. Example: es6
+
+  -v, --verbose
+    Outputs debugging log
+`
+
 const parseArgs = (rawArgs) => {
   const args = arg(
     {
       // Types
+      '--help': Boolean,
       '--user': String,
       '--find': String,
       '--cache-dir': String,
       '--verbose': Boolean,
       // Aliases
+      '-h': '--help',
       '-u': '--user',
       '-f': '--find',
       '-c': '--cache-dir',
@@ -17,6 +41,7 @@ const parseArgs = (rawArgs) => {
     }
   )
   return {
+    help: args['--help'],
     user: args['--user'],
     findParam: args['--find'],
     cacheDir: args['--cache-dir'] || '../.cache',
@@ -26,6 +51,11 @@ const parseArgs = (rawArgs) => {
 
 export const cli = (args) => {
   let options = parseArgs(args)
+
+  if (options.help) {
+    console.log(help)
+    return
+  }
 
   // Trigger the search
   main.search(options);
